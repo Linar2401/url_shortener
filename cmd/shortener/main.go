6f318e61efba06b8020c80cca51563e993d08938
Linar Zagidullin <zagidullinlinar24@gmail.com>
2025-12-17 23:39:06 +0300

    refactoring

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,99 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"math/rand/v2"
 	"net/http"
-	"sync"
+
+	main_handlers "github.com/Linar2401/url_shortener/internal/handlers"
+	main_storage "github.com/Linar2401/url_shortener/internal/storage"
 )
-
-const (
-	charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	codeLen = 6
-)
-
-type URLStore struct {
-	mu    sync.RWMutex      // RWMutex: много читателей, один писатель
-	codes map[string]string // Храним Код -> Строка
-}
-
-func (s *URLStore) SaveURL(value string) string {
-	s.mu.Lock() // Блокируем на запись
-	defer s.mu.Unlock()
-
-	for {
-		// Генерируем случайную строку
-		b := make([]byte, codeLen)
-		for i := range b {
-			b[i] = charset[rand.IntN(len(charset))]
-		}
-		code := string(b)
-
-		// Проверяем на уникальность: если такого ключа нет — сохраняем
-		if _, exists := s.codes[code]; !exists {
-			s.codes[code] = value
-			return code
-		}
-		// Если код занят, цикл пойдет на новый круг
-	}
-}
-
-func (s *URLStore) GetURL(code string) (string, bool) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-
-	val, ok := s.codes[code]
-	return val, ok
-}
-
-func (s *URLStore) CreateHandle(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST", http.StatusBadRequest)
-		return
-	}
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		return
-	}
-
-	shortURL := s.SaveURL(string(body))
-
-	result := fmt.Sprintf("http://localhost:8080/%s", shortURL)
-	w.WriteHeader(http.StatusCreated)
-	_, err = w.Write([]byte(result))
-	if err != nil {
-		return
-	}
-}
-
-func (s *URLStore) GetHandle(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Only GET", http.StatusBadRequest)
-		return
-	}
-	code := r.PathValue("code")
-
-	s.mu.RLock()
-	val, ok := s.codes[code]
-	s.mu.RUnlock()
-
-	if !ok {
-		http.Error(w, "Код не найден", http.StatusBadRequest)
-		return
-	}
-
-	http.Redirect(w, r, val, http.StatusTemporaryRedirect)
-}
 
 func main() {
-	storage := &URLStore{
-		codes: make(map[string]string),
-	}
+	storage := main_storage.New()
+	handlers := main_handlers.New(storage)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, storage.CreateHandle)
-	mux.HandleFunc(`/{code}`, storage.GetHandle)
+	mux.HandleFunc(`/`, handlers.CreateHandle)
+	mux.HandleFunc(`/{code}`, handlers.GetHandle)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,61 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type URLStorer interface {
+	SaveURL(value string) string
+	GetURL(code string) (string, bool)
+}
+
+type Handlers struct {
+	storage URLStorer
+}
+
+func New(storage URLStorer) *Handlers {
+	return &Handlers{
+		storage: storage,
+	}
+}
+
+func (h *Handlers) CreateHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST", http.StatusBadRequest)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+
+	shortURL := h.storage.SaveURL(string(body))
+
+	result := fmt.Sprintf("http://localhost:8080/%s", shortURL)
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write([]byte(result))
+	if err != nil {
+		http.Error(w, "Failed to write response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *Handlers) GetHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET", http.StatusBadRequest)
+		return
+	}
+	code := r.PathValue("code")
+
+	val, ok := h.storage.GetURL(code)
+
+	if !ok {
+		http.Error(w, "Code not found", http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(w, r, val, http.StatusTemporaryRedirect)
+}
