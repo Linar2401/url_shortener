9078dba89dc3e668bf804c67cb3014ccea14b770
Linar Zagidullin <zagidullinlinar24@gmail.com>
2025-12-21 13:40:54 +0300

    refactoring for change default router to chi v2

--- cmd/shortener/shortener.go
+++ cmd/shortener/shortener.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	Handlers "github.com/Linar2401/url_shortener/internal/handler"
 	Storages "github.com/Linar2401/url_shortener/internal/storage"
 	chi "github.com/go-chi/chi/v5"
@@ -17,4 +19,9 @@ func main() {
 
 	r.Post("/", handlers.CreateHandle)
 	r.Get("/{code}", handlers.GetHandle)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		return
+	}
 }
