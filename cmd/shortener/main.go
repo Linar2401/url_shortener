ff51984cb469e65cc140d9b64cd1c1d2a132f473
Linar Zagidullin <zagidullinlinar24@gmail.com>
2026-01-21 14:17:24 +0300

    refactoring v2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,7 +8,11 @@ import (
 )
 
 func main() {
-	cfg := config.Load()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if err := handler.Serve(cfg); err != nil {
 		log.Fatal(err)
 	}
